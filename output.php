7f88901d023cac98902ce1319c2206b16f819d30
jpkravcik <jpkravcik@gmail.com>
2021-09-17 17:59:13 +0100

    updated output.html and added a style sheet: output-styles.html

---
+++ output.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<link rel="stylesheet" href = "output-styles.css">
+
+</head>
+
+<header>
+  <div>
+    <h1>Output Page</h1>
+
+    <h3 align="right" valign="top">Username</h3>
+  </div>
+</header>
+<body>
+  <div class = "bracket">
+    <!-- Round 1<!-->
+    <div class = "round">
+      <div class = "matchup">
+        <div class = "entry">entry 1</div>
+        <div class = "entry">entry 2</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 3</div>
+        <div class = "entry">entry 4</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 5</div>
+        <div class = "entry">entry 6</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 7</div>
+        <div class = "entry">entry 8</div>
+      </div>
+    </div>
+
+  <!-- Round 2 -->
+    <div class = "round">
+      <div class = "matchup">
+        <div class = "entry">entry 1</div>
+        <div class = "entry">entry 4</div>
+      </div>
+      <!-- bad but simple way to center the second round between the matchups -->
+      <div class="buffer"></div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 6</div>
+        <div class = "entry">entry 8</div>
+      </div>
+    </div>
+
+    <div class = "round">
+      <div class = "matchup">
+        <div class = "entry">entry 1</div>
+        <div class = "entry">entry 8</div>
+      </div>
+    </div>
+
+    <div class = "round">
+      <div class = "matchup">
+        <div class = "entry">entry 1</div>
+      </div>
+    </div>
+
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/vglHpvcK-Jk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+  </div>
+</body>
+</html>
