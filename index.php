5d11a67f21c6299c12fcc0c1e92e4495e8252c86
Crisostomol <60791474+Crisostomol@users.noreply.github.com>
2021-09-18 00:13:38 -0500

    updated input html

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chartset="utf-8">
+    <title>Team Seeder</title>
+    <table cellspacing="10">
+            <tr>
+                <td><a href="#">Log in</a></td>
+                <td><a href="#">Sign up</a></td> 
+            </tr> <!--Might have to redo this or remove the table-->
+    </table>
+</head>
+<body>
+<header>
+    <h1>Weolcome to Team Seed</h1>
+    <p>Welcome Teams and Players!</p>
+    <p>TeamSeed is an automatic bracket generator based on the given seeds of players! 
+        Our aim is to make creating a tournament convenient and easy for everyone!</p>
+    <hr>
+    <p>Enter team players by ranking</p>
+    
+    <h3 style="text-align:left;float:left;width:190px">Team 1</h3>
+    <h3 style="text-align:left;float:left;width:170px">Team 2</h3>
+    <h3 style="text-align:left;float:left;width:150px">Team 3</h3>
+    <h3 style="text-align:left;float:left;">Team 4</h3>
+    <div style="clear:both;"></div>
+
+<form>
+        <input type="text" placeholder="1.1">
+        <input type="text" placeholder="2.1">
+        <input type="text" placeholder="3.1">
+        <input type="text" placeholder="4.1">
+     <br></br>
+
+        <input type="text" placeholder="1.2">
+        <input type="text" placeholder="2.2">
+        <input type="text" placeholder="3.2">
+        <input type="text" placeholder="4.2">
+    <br> </br>
+        <input type="text" placeholder="1.3">
+        <input type="text" placeholder="2.3">
+        <input type="text" placeholder="3.3">
+        <input type="text" placeholder="4.3">
+   
+    <br></br>
+        <input type="text" placeholder="1.4">
+        <input type="text" placeholder="2.4">
+        <input type="text" placeholder="3.4">
+        <input type="text" placeholder="4.4">
+    <p>
+    <button type="button" style="background-color:#ffff99;">
+          Start</button>
+    </p>
+</form>
+
+<!--Need to come back and add values to link with backend/PHP-->
+
+</header>
+</body>
+</html>
