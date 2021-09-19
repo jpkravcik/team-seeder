36bcd2e825b82aea272cd198ad3b5e88130f6ade
jpkravcik <jpkravcik@gmail.com>
2021-09-19 14:36:05 +0100

    expanded the bracket to 16 entries and cleaned up code

--- output.php
+++ output.php
@@ -8,7 +8,7 @@
   <div>
     <h1>Output Page</h1>
 
-    <h3 align="right" valign="top">Username</h3>
+    <h2 align="right" valign="top">Username</h2>
   </div>
 </header>
 <body>
@@ -34,6 +34,26 @@
         <div class = "entry">entry 7</div>
         <div class = "entry">entry 8</div>
       </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 9</div>
+        <div class = "entry">entry 10</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 11</div>
+        <div class = "entry">entry 12</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 13</div>
+        <div class = "entry">entry 14</div>
+      </div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 15</div>
+        <div class = "entry">entry 16</div>
+      </div>
     </div>
 
   <!-- Round 2 -->
@@ -42,6 +62,7 @@
         <div class = "entry">entry 1</div>
         <div class = "entry">entry 4</div>
       </div>
+
       <!-- bad but simple way to center the second round between the matchups -->
       <div class="buffer"></div>
 
@@ -49,21 +70,53 @@
         <div class = "entry">entry 6</div>
         <div class = "entry">entry 8</div>
       </div>
+
+      <div class="buffer"></div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 9</div>
+        <div class = "entry">entry 11</div>
+      </div>
+
+      <div class="buffer"></div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 14</div>
+        <div class = "entry">entry 16</div>
+      </div>
     </div>
 
+    <!-- round 3 -->
     <div class = "round">
       <div class = "matchup">
         <div class = "entry">entry 1</div>
         <div class = "entry">entry 8</div>
       </div>
+
+      <div class="buffer3"></div>
+
+      <div class = "matchup">
+        <div class = "entry">entry 9</div>
+        <div class = "entry">entry 16</div>
+      </div>
     </div>
 
+    <!-- round 4 -->
+    <div class = "round">
+      <div class = "matchup">
+        <div class = "entry">entry 1</div>
+        <div class = "entry">entry 9</div>
+      </div>
+    </div>
+
+    <!-- winner -->
     <div class = "round">
       <div class = "matchup">
         <div class = "entry">entry 1</div>
       </div>
     </div>
 
+    <!-- video to be replaced with intro video -->
     <div><iframe width="560" height="315" src="https://www.youtube.com/embed/vglHpvcK-Jk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 
   </div>
