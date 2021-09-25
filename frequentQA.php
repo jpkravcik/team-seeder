27a3ffe90532380d62651b50ec8f6efe79d81a9f
Mally <mtzuoo@gmail.com>
2021-09-24 22:46:54 -0500

    Typo

--- frequentQA.php
+++ frequentQA.php
@@ -4,8 +4,8 @@
 <h1>Frequently Asked Questions</h1>
 <h3><a name="toc">Who are you?</a></h3>
 <ul>
-<p>We at team seeder are a group of university students who created this software as a "capstone project"</p><p>  </p>
+<p>We at team seeder are a group of university students who created this software as a "capstone project".</p><p>  </p>
 </ul>
 <h2><a name="who">Who would use this?</a></h2>
 <ul>
-<p>Our primary audience are those who need to create an easy-to-use and powerful tool for seeding tournaments with multiple teams</p>
+<p>Our primary audience are those who need to create an easy-to-use and powerful tool for seeding tournaments with multiple teams.</p>
