53580a40747d1ce020b24be6a9d3e951f75794b0
jpkravcik <jpkravcik@gmail.com>
2021-10-04 07:07:49 +0100

    Added TOS page and contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+const Contact = () => {
+    return (
+        <div className="container">
+            <h1>
+                Contact Us
+            </h1>
+
+            <textarea id="contact" rows="8" cols="40"> </textarea>
+            <button type="submit" className="defaultButton">submit</button>
+            
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Tos.js
@@ -0,0 +1,30 @@
+const Tos = () => {
+    return (
+        <div className="container">
+            <h1>
+                Terms of Service
+            </h1>
+            <div className="siteDescription">
+                
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Integer ac convallis nisi. Aenean convallis malesuada suscipit. 
+                    Nullam bibendum semper ex ut tristique. Nam sodales tristique tempus. 
+                    Praesent vel euismod metus. Donec laoreet felis nec dui tempus, 
+                    quis pulvinar lorem sollicitudin. Etiam sollicitudin fermentum pharetra. 
+                    Maecenas mollis arcu ut neque semper commodo. Nam sed fermentum elit. 
+                    Pellentesque non dolor nulla.
+
+                    Aliquam auctor lectus at euismod scelerisque. Quisque finibus justo 
+                    posuere mollis mattis. Donec tincidunt est leo, a luctus dolor scelerisque 
+                    et. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                    per inceptos himenaeos. Sed ut odio semper dui egestas molestie. Proin 
+                    tristique metus in aliquam condimentum. Nullam malesuada erat magna, sed auctor 
+                    est mollis commodo. Nulla auctor nulla at mauris iaculis vehicula. Phasellus 
+                    mollis eleifend dui, vitae posuere odio pretium vitae.
+                    
+            </div>
+        </div>
+    )
+}
+
+export default Tos
